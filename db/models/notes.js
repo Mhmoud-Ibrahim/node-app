e609f0433647e630e8061f427a3d93b580920458
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2024-06-15 11:46:57 +0300

    d

--- db/models/notes.js
+++ db/models/notes.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose";
  const noteScehma = new mongoose.Schema({
     title:String,
     content:String,
-    createdBy:{
-        type:mongoose.Types.ObjectId,
-        ref:'user'
-    }
+  
     
 },{timestamps:true})
  const noteModel = mongoose.model('note',noteScehma)
