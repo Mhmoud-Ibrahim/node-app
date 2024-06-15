0835e7c0c0b208819bca62f66b768f612b8c85bf
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2024-06-15 11:53:39 +0300

    ll

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ app.use(express.json())
 import dotenv from 'dotenv'
 dotenv.config()
 import dbConnections from './db/dbConnections.js'
-import noteRouter from './db/models/src/modules/notes/notes.routes.js'
-app.use('/',noteRouter)
 
 app.get('/',(req,res)=>{
     res.json({message:"success"})
