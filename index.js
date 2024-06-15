5f6f555c07461f33db79c5b608b46c61751b75b0
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2024-06-15 11:28:26 +0300

    s

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 import dbConnections from './db/dbConnections.js'
 import noteRouter from './db/models/src/modules/notes/notes.routes.js'
+import noteModel from './db/models/notes.js'
 app.use(noteRouter)
-app.get('/', (req, res) => res.send('Hello World mahmoud!'))
+app.get('/', async(req, res) =>{
+        const notes =  await  noteModel.find()
+         res.json({message:"success",notes})
+      }
+)
 app.get('/notes', (req, res) => res.send('notes joy!'))
 
 
